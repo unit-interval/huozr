3146c49c939c3238dc96d2eebb527f294307f48c
ZHANG Ta <coolgene@gmail.com>
2011-08-05 14:49:20 +0800

    basic sign-up function finished

--- inc/users_functions.php
+++ inc/users_functions.php
@@ -3,25 +3,64 @@
 /**
  * users' registeration, login related functions 
  */
-function register_from_form($username, $passwd, $screen_name) {
-	echo "$username <br />$passwd <br />$screen_name <br />";
-	if(user_exists($username))
-			err_redir("用户名($username)已注册，请直接登录");
-	echo "建立user<br />";
-	echo "建立userlogin<br />";
-	echo "转到已经登陆<br />";
+
+
+//create a new user (without any auths): use screen name.
+function user_create($n){
+	global $db;
+	$query = "insert into `users` (`screen_name`) values ('{$db->real_escape_string($n)}')";
+	if($db->query($query) !== TRUE)
+		die("db error({$db->errno}).");
+	return $db->insert_id;
 }
 
+//verify username exists: use username
 function user_exists($n) {
 	global $db;
-	$query = "select * from `users_login`
-		where `username` = '{$db->real_escape_string($n)}'";
+	$query = "select * from `user_basic_auth`
+		where `login_name` = '{$db->real_escape_string($n)}'";
 	if(!($result = $db->query($query)))
-		err_redir("db error({$db->errno}). query:$query", '/error.php');
+		die("db error({$db->errno}). query:$query");
 	if ($result->num_rows > 0) {
 		$row = $result->fetch_assoc();
 		return $row;
 	}
 	return false;
+}
+
+//verify basic auth link exists: use user_id
+function user_basic_auth_exists($uid){
+	global $db;
+	$query = "select * from `user_basic_auth`
+		where `user_id` = '{$db->real_escape_string($uid)}'";
+	if(!($result = $db->query($query)))
+		die("db error({$db->errno}). query:$query");
+	if ($result->num_rows > 0) {
+		$row = $result->fetch_assoc();
+		return $row;
+	}
+	return false;
+}
+
+//set a basic auth link (username and password pair) for a existing user, use: uid, new username, new password
+function user_basic_auth($uid, $username, $passwd){
+	global $db;
+	if(user_basic_auth_exists($uid))
+		die("用户($uid)已经设置过用户名和密码，如需更改，请使用用来修改用户名或者密码的方法");
+	$query = "insert into `user_basic_auth` (`user_id`, `login_name`, `passwd`) values (
+	'{$db->real_escape_string($uid)}',
+	'{$db->real_escape_string($username)}',
+	'". md5(SALT_PW . $passwd) ."')";
+	if($db->query($query) !== TRUE)
+		die("db error({$db->errno}).");
+}
+	
+
+//create a new user (with basic password auth):use username, password, screen name.
+function user_create_basic($username, $passwd, $screen_name) {
+	if(user_exists($username))
+		die("用户名($username)已注册，请直接登录");
+	user_basic_auth(user_create($screen_name),$username,$passwd);
+	echo "转到已经登陆<br />";
 }
 
--- register.php
+++ register.php
@@ -1,12 +1,10 @@
 <?php
 include 'config.php';
-include DIR_INC . 'database.php';
-include DIR_INC . 'functions.php';
-include DIR_INC . 'users_functions.php';
+include DIR_INC . '/users_functions.php';
 
 
-if (isset($_POST['username']))
-	register_from_form($_POST['username'], $_POST['passwd'], $_POST['screen_name']);
+if ($_POST['username']!='')
+	user_create_basic($_POST['username'], $_POST['passwd'], $_POST['screen_name']);
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -17,6 +15,7 @@ if (isset($_POST['username']))
 </head>
 
 <body>
+<h1>注册－活字网</h1>
 <form id="form1" name="form1" method="post" action="register.php">
   <label>
     <input type="text" name="username" id="username" />
