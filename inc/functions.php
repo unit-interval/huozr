1d0c91ec55f5e4ad1faea0e0d23a479922ea0b65
Tao <tao@huangtao.me>
2011-08-06 11:50:56 +0800

    user error_log instead of writing to file

--- inc/functions.php
+++ inc/functions.php
@@ -2,10 +2,13 @@
 
 /**
  * error handling
+ * make sure error logging work on live site.
  */
 function err_warn($msg) {
-	if (! DEV_ENV)
+	if (! DEV_ENV) {
+		error_log(date('c') . " Warning: {$_SERVER['REQUEST_URI']}: $msg");
 		return;
+	}
 	echo "<hr /><p><h3><em>Warning: </em>$msg</h3></p><pre>";
 //	print_r(get_defined_vars());
 //	print_r($_SESSION);
@@ -22,11 +25,7 @@ function err_fatal($msg) {
 		echo "</pre>";
 		die;
 	}
-// TODO: make error log work on live site.
-	if ($handle = fopen(DIR_ROOT . '/tmp/huozr-error.log', 'a')) {
-		fputcsv($handle, array(date('c'), 'FATAL', $_SERVER['REQUEST_URI'], $msg));
-		fclose($handle);
-	}
+	error_log(date('c') . " FATAL: {$_SERVER['REQUEST_URI']}: $msg");
 	err_redir();
 }
 function err_redir() {
