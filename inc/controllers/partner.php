543ec674671c5f1c5b85ee84c4b68012f036565e
Zilin Jiang <libragold@gmail.com>
2011-08-11 15:28:46 +0800

    try to build a frame for partner, but i do not why it fails

--- inc/controllers/partner.php
+++ inc/controllers/partner.php
@@ -2,14 +2,12 @@
 
 include_once DIR_INC . '/func-auth.php';
 
-/*
- if(partner_login_verify()){
+if(partner_login_verify()){
 	header('Location: /partner/home');	
 	die();
 }
- */
 
-$res['head_title'] = '登录';
-$res['body_id'] = 'login';
+$res['head_title'] = '打印店';
+$res['body_id'] = 'index';
 
 js_script_tags('p-huozr');
